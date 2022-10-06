83dfd9676ba7fed672c184cd7cc0b272eaa0c03b
Vitor Cesar <vitor@fasters.com.br>
2022-10-05 22:49:16 -0300

    feat: implement dark mode on app.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+
+import { Home } from "./src/pages/Home";
+import { darkTheme, lightTheme } from "./src/theme";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_500Medium });
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const themeToggler = (): void => {
+    const actualTheme = theme;
+    setTheme(actualTheme === "dark" ? "light" : "dark");
+  };
+
+  const activeTheme = theme === "light" ? lightTheme : darkTheme;
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <ThemeProvider theme={activeTheme}>
+        <Home />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    );
+  }
+}
