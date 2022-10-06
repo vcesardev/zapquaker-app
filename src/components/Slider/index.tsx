7852f86037eedafc22ac403a3099e1fe22d09e87
Vitor Cesar <vitor@fasters.com.br>
2022-10-05 22:47:05 -0300

    feat: create slider

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useState } from "react";
+import { View } from "react-native";
+import { Slider } from "@miblanchard/react-native-slider";
+
+import * as Styled from "./styled";
+
+type BaseSliderProps = {
+  currentValue: number;
+  max: number;
+  onChangeValue: () => void;
+};
+
+const BaseSlider: React.FC<BaseSliderProps> = ({
+  max,
+  onChangeValue,
+  currentValue,
+}) => {
+  return (
+    <Styled.Container>
+      <Slider
+        value={currentValue}
+        onValueChange={onChangeValue}
+        maximumValue={max}
+        minimumValue={1}
+        thumbStyle={{ backgroundColor: "#1d4ed8", borderRadius: 2, width: 22 }}
+        trackStyle={{ backgroundColor: "#d1d5db", paddingTop: 10 }}
+        maximumTrackTintColor={"#d1d5db"}
+        minimumTrackTintColor={"#d1d5db"}
+      />
+    </Styled.Container>
+  );
+};
+
+export default BaseSlider;
