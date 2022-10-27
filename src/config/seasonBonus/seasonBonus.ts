aa32b249639f73d6d16bb26c5b43f5e6b7f95f10
Vitor Cesar <vitor@fasters.com.br>
2022-10-27 12:06:14 -0300

    feat: add season bonus config

---
+++ src/config/seasonBonus/seasonBonus.ts
@@ -0,0 +1,28 @@
+export type BonusValues = {
+  id: string;
+  value: number;
+  text: string;
+};
+
+export const bonusValues: BonusValues[] = [
+  {
+    id: "361631263127",
+    value: 0,
+    text: "0%",
+  },
+  {
+    id: "361631263132127",
+    value: 0.1,
+    text: "10%",
+  },
+  {
+    id: "36163434231263127",
+    value: 0.15,
+    text: "15%",
+  },
+  {
+    id: "361631323231263127",
+    value: 0.2,
+    text: "20%",
+  },
+];
