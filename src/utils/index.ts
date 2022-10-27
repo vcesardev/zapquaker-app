7a2b2aa0e4790ca19b5da4f6528bed4285d65008
Vitor Cesar <vitor@fasters.com.br>
2022-10-27 12:07:41 -0300

    feat: add new utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,4 +30,47 @@ const emptyToNull = (obj: Record<string, unknown>): Record<string, unknown> => {
   return obj;
 };
 
-export { emptyToNull, fontScale, heightPercentageToDP, widthPercentageToDP };
+const formatNumber = (value: number): string => {
+  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+};
+
+const calculatePercentageValue = (
+  value: number,
+  percentage: number
+): string => {
+  const percentageValue = value * percentage;
+  const valueFormatted = formatNumber(value - percentageValue);
+  return valueFormatted;
+};
+
+const calculatePercentageTimeMinutes = (
+  minutes: number,
+  percentage: number
+): string => {
+  const percentageTime = minutes - minutes * percentage;
+  const days = Math.floor(percentageTime / (60 * 24));
+  const hours = Math.floor((percentageTime % (60 * 24)) / 60);
+  const minutesLeft = Math.floor(percentageTime % 60);
+
+  const displayDays = days > 0 ? days + (days === 1 ? " Dia" : " Dias") : "";
+  const displayHours =
+    hours > 0 ? hours + (hours === 1 ? " Hora" : " Horas") : "";
+  const displayMinutes =
+    minutesLeft > 0
+      ? "e " + minutesLeft + (minutesLeft === 1 ? " Minuto " : " Minutos")
+      : "";
+
+  return `${displayDays}${
+    hours > 0 && days > 0 ? "," : ""
+  } ${displayHours} ${displayMinutes}`;
+};
+
+export {
+  emptyToNull,
+  fontScale,
+  heightPercentageToDP,
+  widthPercentageToDP,
+  formatNumber,
+  calculatePercentageValue,
+  calculatePercentageTimeMinutes,
+};
